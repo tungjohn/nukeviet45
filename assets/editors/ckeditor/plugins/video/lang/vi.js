342971084ad305b6c93ae41abcc84754bc33ad96
Trần Thanh Tùng <thanhtung@vinades.vn>
2025-09-25 22:46:08 +0700

    nukeviet 4.5

---
+++ assets/editors/ckeditor/plugins/video/lang/vi.js
@@ -0,0 +1,18 @@
+﻿/**
+ * @ckeditor-plugin-googledoc language file
+ * @Author VINADES.,JSC <contact@vinades.vn>
+ * @Copyright (C) 2014 VINADES.,JSC. All rights reserved
+ * @Language Tiếng Việt
+ * @License GNU/GPL version 2 or any later version
+ * @Createdate Sat, 08 Feb 2014 06:33:39 GMT
+ */
+
+CKEDITOR.plugins.setLang('video', 'vi', {
+    button: 'Video',
+    title: 'Thuộc tính video',
+    emptySrc: 'URL không được để trống.',
+    controls: 'Bật các nút điều khiển',
+    mutedLoopingAutoplay: 'Thêm thuộc tính tắt tiếng, lặp lại, tự động phát',
+    preview: 'Xem trước',
+    invalidSrc: 'URL không hợp lệ hoặc video không phát được.'
+});
---
+++ assets/js/site.js
@@ -0,0 +1,34 @@
+/**
+ * NukeViet Content Management System
+ * @version 4.x
+ * @author VINADES.,JSC <contact@vinades.vn>
+ * @copyright (C) 2009-2022 VINADES.,JSC. All rights reserved
+ * @license GNU/GPL version 2 or any later version
+ * @see https://github.com/nukeviet The NukeViet CMS GitHub project
+ */
+
+function formXSSsanitize(form) {
+    $(form).find("input, textarea").not(":submit, :reset, :image, :file, :disabled").not('[data-sanitize-ignore]').each(function() {
+        $(this).val(DOMPurify.sanitize($(this).val(), {}));
+    });
+}
+
+$(function() {
+    $('body').on('click', '[type=submit]:not([name])', function(e) {
+        var form = $(this).parents('form');
+        if (XSSsanitize && !$('[name=submit]', form).length) {
+            // Khi không xử lý XSS thì trình submit mặc định sẽ thực hiện
+            e.preventDefault();
+
+            // Đưa CKEditor 5 trình soạn thảo vào textarea trước khi submit
+            $(form).find("textarea").each(function() {
+                if (this.dataset.editorname && window.nveditor && window.nveditor[this.dataset.editorname]) {
+                    $(this).val(window.nveditor[this.dataset.editorname].getData());
+                }
+            });
+
+            formXSSsanitize(form);
+            $(form).submit();
+        }
+    });
+})
